f96c2500d9d4b378264ce30a2674a0df60447824
marciorps <marciorps@yahoo.com,.br>
2023-11-28 14:40:50 -0300

    com_break

--- mini_game_turtle.py
+++ mini_game_turtle.py
@@ -15,25 +15,43 @@ while True:
     
     if direcao == 'f' and rotacao == 'n':
         t.forward(pixel)
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break    
     elif direcao == 'f' and rotacao == 'd':
         direita = int(input('Quanto para direita devemos rotacionar?'))
         t.right(direita)
         t.forward(pixel)
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break 
     elif direcao == 'f' and rotacao == 'e':
         esquerda = int(input('Quanto para esquerda devemos rotacionar?'))
         t.left(esquerda)
         t.forward(pixel)
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break 
         
     elif direcao == 't' and rotacao == 'n':
         t.backward(pixel)
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break 
     elif direcao == 't' and rotacao == 'd':
         esquerda = int(input('Quanto para direita devemos rotacionar?'))
         t.left(esquerda)
         t.backward(pixel)
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break 
     elif direcao == 't' and rotacao == 'e':
         direita = int(input('Quanto para esquerda devemos rotacionar?'))        
         t.right(direita)
         t.backward(pixel) 
+        continuacao = input('Devemos continuar? "s"(sim) e "n"(não)')
+        if continuacao == 'n':
+            break 
 
     
         
