8bd001585b81727cf341e2580581b6cead036a3a
marciorps <marciorps@yahoo.com,.br>
2023-12-01 11:21:17 -0300

    minigame_codgolimpo_profe

--- mini_game_turtle.py
+++ mini_game_turtle.py
@@ -3,31 +3,39 @@ from turtle import Turtle
 t = Turtle()
 t.speed(1)
 
-def lados():
-
-    if movimentar_para_lado == 'd':
-            angulo = int(input('Quanto para direita devemos rotacionar?'))
-            t.right(angulo)
-    elif movimentar_para_lado == 'e':
-            angulo = int(input('Quanto para esquerda devemos rotacionar?'))
-            t.left(angulo)
-    t.forward(distancia)
     
+def obter_distancia():
+    resposta = int(input('Quantos pixels devemos movimentar para a frente?'))
+    return resposta
+
+def rotacionar_turtle(turtle):
+    movimentar_para_o_lado = input('Rotacionar para d: direita, e: esquerda, n: não rotacionar')
+    if movimentar_para_o_lado == 'd':
+        rotacionar_para_direita(turtle)
+    elif movimentar_para_o_lado == 'e':
+        rotacionar_para_esquerda(turtle)
+
+def rotacionar_para_direita(turtle):
+    angulo = int(input('Quanto para direita devemos rotacionar?'))
+    t.right(angulo)
+
+def rotacionar_para_esquerda(turtle):
+    angulo = int(input('Quanto para esquerda devemos rotacionar?'))
+    t.left(angulo)
 
 while True:
     
-    direcao = input('Para qual direção devemos ir? "f:frente" ou "t:trás :"') 
+    direcao = input('Para qual direção devemos ir? "f:frente" ou "t:trás :"')
 
     if direcao == 'f':
-        distancia = int(input('Quantos pixels devemos movimentar para frente?'))
-        movimentar_para_lado = input('Rotacionar para d:direita, e: esquerda, n:não rotacionar')
-        lados()
+        distancia = obter_distancia()
+        rotacionar_turtle(t)
+        t.forward(distancia)
 
-    else:
-        direcao == 't'
-        distancia = int(input('Quantos pixels devemos movimentar para trás?'))
-        movimentar_para_lado = input('Rotacionar para d:direita, e: esquerda, n:não rotacionar')
-        lados() 
+    if direcao == 't':
+        distancia = obter_distancia()
+        rotacionar_turtle(t)
+        t.backward(distancia) 
 
     resposta = input('Continuar andando?')
     if resposta not in ('sim', 's'):
